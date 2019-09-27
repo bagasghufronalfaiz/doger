a188ef2943c7c48b703f0dd1fe3e27b45a18a9c2
Bagas Ghufron Alfaiz <bagas.al@students.amikom.ac.id>
2019-09-27 23:46:22 +0700

    update get title

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -26,14 +26,14 @@ class HomeController extends Controller
      */
     public function index()
     {
-        //cron job
+        // cron job
         // $dummy = Jajal::create([
         //     'index' => 'home'
         // ]);
         // return view('home');
 
         // wordpress API
-        $damin = 'supercampquin.com';
+        // $damin = 'supercampquin.com';
         // $hasil = self::get_wp_categories($damin);
         // // $kacang = var_dump(json_decode($hasil));
         // $panjang = json_decode($hasil, true);
@@ -42,15 +42,22 @@ class HomeController extends Controller
         // return $itung;
 
         // //get total post
+        // $damin = 'besthdwallpaper.co';
         // $header = self::get_wp_post_count($damin);
         // return $header;
 
         // get total pages
-        // $damin = 'wawallletters.com';
-        // $saming = 'bestwallpapers.co';
-        $header = self::get_wp_page_count($damin);
-        return $header;
-
+        // $damin = 'besthdwallpaper.co';
+        // $jumlah_page = self::get_wp_page_count($damin);
+        
+        // // get wp pages title
+        // $damin = 'besthdwallpaper.co';
+        // $coba = 'worldivided.com';
+        // $jumlah_page = self::get_wp_page_count($damin);
+        // for($i=1;$i<=$jumlah_page;$i++){
+        //     $hasil[$i] = self::get_wp_pages_title($damin, $i);
+        // }
+        // return $hasil[1] . ' dan ' . $hasil[2] . ' dan ' . $hasil[3] . ' dan ' . $hasil[4];
 
 
     }
@@ -79,6 +86,8 @@ class HomeController extends Controller
         //return self::get_string_between($hasil, '<div class="sd" id="resultStats">Sekitar ', ' hasil</div>');
     }
 
+
+
     private function get_wp_categories($domain)
     {
         $client = new Client();
@@ -88,6 +97,8 @@ class HomeController extends Controller
 
         return $hasil;
     }
+
+
 
     private function get_wp_post_count($domain)
     {
@@ -100,6 +111,8 @@ class HomeController extends Controller
         return $header;
     }
 
+
+
     private function get_wp_page_count($domain)
     {
         $client = new Client();
@@ -109,6 +122,18 @@ class HomeController extends Controller
         $hasil = $res->getBody();
 
         return $header;
+    }
+
+
+    
+    private function get_wp_pages_title($domain, $total_pages){
+        $client = new Client();
+        $url = 'http://' . $domain . '/wp-json/wp/v2/pages?per_page=1&page='.$total_pages;
+        $res = $client->request('GET', $url, ['headers' => ['User-Agent' => 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36']]);
+        $hasil = $res->getBody();
+        $kacang = json_decode($hasil, true);
+        $panjang = $kacang[0]['title']['rendered'];
+        return $panjang;
     }
 
 
