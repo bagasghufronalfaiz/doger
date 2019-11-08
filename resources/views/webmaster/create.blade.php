b828e972c47654f8db8352d4da8e64b64fa4296b
bagasghufronalfaiz <bagasghufronalfaiz@users.noreply.github.com>
2019-11-08 19:52:24 +0700

    update footer

--- resources/views/webmaster/create.blade.php
+++ resources/views/webmaster/create.blade.php
@@ -283,7 +283,7 @@
                           <label for="name">Name</label>
                           <input class="form-control" placeholder="Name" type="text" name="name" value="{{old('name')}}">
                       </div>
-                    </div>                    
+                    </div>
                     <div class="col-md-12">
                       <div class="form-group">
                           <label for="email">Email</label>
@@ -310,30 +310,10 @@
       </div>
       <!-- Footer -->
       <footer class="footer pt-0">
-        <div class="row align-items-center justify-content-lg-between">
-          <div class="col-lg-6">
-            <div class="copyright text-center text-lg-left text-muted">
-              &copy; 2019 <a href="https://www.creative-tim.com" class="font-weight-bold ml-1" target="_blank">Creative Tim</a>
-            </div>
-          </div>
-          <div class="col-lg-6">
-            <ul class="nav nav-footer justify-content-center justify-content-lg-end">
-              <li class="nav-item">
-                <a href="https://www.creative-tim.com" class="nav-link" target="_blank">Creative Tim</a>
-              </li>
-              <li class="nav-item">
-                <a href="https://www.creative-tim.com/presentation" class="nav-link" target="_blank">About Us</a>
-              </li>
-              <li class="nav-item">
-                <a href="http://blog.creative-tim.com" class="nav-link" target="_blank">Blog</a>
-              </li>
-              <li class="nav-item">
-                <a href="https://www.creative-tim.com/license" class="nav-link" target="_blank">License</a>
-              </li>
-            </ul>
-          </div>
+        <div class="copyright text-center text-lg-left text-muted">
+            &copy; 2019 <a href="{{ url('/') }}" class="font-weight-bold ml-1" target="_blank">{{ config('app.name', 'Laravel') }}</a>
         </div>
-      </footer>
+    </footer>
     </div>
 @endsection
 
